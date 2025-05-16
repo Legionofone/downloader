225e01b51fb8a0b9dbc6f7438ae969e823251c37
Legionofone <richard@rhc2.com>
2025-05-16 10:09:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,20 @@ def download_torrent(dl_url):
     if "Content-Disposition" in response.headers:
         content_disposition = response.headers["Content-Disposition"]
         filename = (re.findall('filename=(.+)', content_disposition)[0]).replace('"', "")
-        print(f"Filename: {filename}")
+        print(f"Filename: {filename}", flush=True )
     else:
         filename = ''
-        print("Content-Disposition header not found.")
+        print("Content-Disposition header not found.", flush=True )
     if response.status_code == 200:
         with open(file_path+filename, "wb") as file:
             file.write(response.content)
-        print(f"File downloaded successfully to {file_path}")
+        print(f"File downloaded successfully to {file_path}", flush=True )
     else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+        print(f"Failed to download file. Status code: {response.status_code}", flush=True )
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(url: str = 200):
+    print(f"API hit recieved with URL {url}", flush=True )
     download_torrent(url)
     return """
     <html>
